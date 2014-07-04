b027b4d6a713d79a45781a5067890f8a18c38396
GuifreSanchez <guifre.sanchez@gmail.com>
2014-07-04 12:55:18 -0400

    Update Leapfrog.cpp
    Energy Calculation.
    File generation.

--- Leapfrog.cpp
+++ Leapfrog.cpp
@@ -3,83 +3,154 @@
 #include <string>
 #include <math.h>
 #include <algorithm>
+#include <fstream>
 using namespace std;
-typedef vector < vector <double> > mat;
+double d = 3, p = 4, G = 10;
+// Defining body class
+ofstream results("results.csv");
+ofstream energy("energy.txt");
+ofstream init("init.csv");
 class Body {
 public:
-  double m;
   vector <double> r;
   vector <double> v;
-  vector <double> a;
-  void set_r(vector <double>);
-  void set_v(vector <double>);
-  void set_a(vector <double>);
-  void set_m(double);
+  vector <double> f;
+  double m;
 };
-void Body::set_r(vector <double> x) {
-  r = x;
-}
-void Body::set_v(vector <double> x) {
-  v = x;
-}
-void Body::set_a(vector <double> x) {
-  a = x;
-}
-void Body::set_m(double x) {
-  m = x;
-}
-void print_vec(vector <double> vec) {
-  for (int i = 0; i < vec.size(); i++) {
-    if (i =! 0) cout << ",";
-    cout << vec[i];
-  }
-  cout << endl;
-}
-void print_mat(mat m) {
-  for (int i = 0; i < m.size(); i++) print_vec(m[i]);
-}
-double random(int max, int min, int precision) { 
-  min *= pow(10,precision);
-  max *= pow(10,precision);
+// Function for random numbers given a maximum and a minimum. Precision says how many decimals you want.
+double random(int max, int min, int precision) {
+  precision = pow(10,precision);
+  max *= precision;
+  min *= precision;
   double result = rand() % (max - min) + min;
-  result /= pow(10,precision);
+  result /= precision;
   return result;
 }
+// Function for relative vector
+vector <double> rel_pos(Body b1, Body b2) {
+  vector <double> result(d,0);
+  for (int i = 0; i < d; i++) result[i] = b2.r[i] - b1.r[i];
+  return result;
+}
+// Function for vector magnitude
+double m_vec(vector <double> a) {
+  double m = 0;
+  for (int i = 0; i < d; i++) m += (a[i] * a[i]);
+  return sqrt(m);
+}
+vector <double> force(Body b1, Body b2) {
+  vector <double> F(d,0);
+  // e Needed to soft the system, force getting values close to infinity when distance approx. 0.
+  double e = 100;
+  double d3 = pow(m_vec(rel_pos(b1,b2)),3);
+  for (int i = 0; i < d; i++) F[i] = G * b1.m * b2.m * rel_pos(b1,b2)[i] / (d3 + e);
+  return F;
+}
+vector <double> s_vec(vector <double> a, vector <double> b) {
+  vector <double> result(d,0);
+  for (int i = 0; i < d; i++) result[i] = a[i] + b[i];
+  return result;
+}
+vector <double> p_vec(vector <double> a, double n) {
+  vector <double> result(d,0);
+  for (int i = 0; i < d; i++) result[i] = a[i] * n;
+  return result;
+}
+void print_vec(vector <double> a, char opt) {
+  for (int i = 0; i < a.size(); i++) {
+    if (i != 0) {
+      if (opt == 'r') results << ",";
+      if (opt == 's') init    << ",";
+    }
+    if (opt == 'r') results << a[i];
+    if (opt == 's') init    << a[i];
+  }
+  if (opt == 'r') results << endl;
+  if (opt == 's') init    << endl;
+}
+void print_system(Body System[], double n, char opt, char file) {
+  for (int i = 0; i < n; i++) {
+    if (opt == 'r') print_vec(System[i].r,file);
+    if (opt == 'v') print_vec(System[i].v,file);
+    if (opt == 'f') print_vec(System[i].f,file);
+  }
+} 
+double E_pot(Body System[], double n) {
+  double E = 0;
+  for (int i = 0; i < n; i++) {
+    for (int j = i + 1; j < n; j++) E -= G * System[i].m * System[j].m / m_vec(rel_pos(System[i],System[j]));
+  }
+  return E;
+}
+double E_kin(Body System[], double n) {
+  double E = 0;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < d; j++) E += 0.5 * System[i].m * System[i].v[j] * System[i].v[j];
+  }
+  return E;
+}
+vector <double> empty(vector <double> a) {
+  for (int i = 0; i < d; i++) a[i] = 0;
+  return a;
+}
 int main() {
-  cout.setf(ios::fixed);
-  cout.precision(4);
   int n;
+  results.setf(ios::fixed);
+  results.precision(p);
+  energy.setf(ios::fixed);
+  energy.precision(p);
+  init.setf(ios::fixed);
+  init.precision(p);
+  cout.setf(ios::fixed);
+  cout.precision(p);
   cin >> n;
   Body System[n];
-  double m = 0;
-  double r_max = 1000;
-  double r_min = 0;
-  double v_max = 10;
-  double v_min = -10;
-  double m_max = 1000;
-  double m_min = 100;
-  vector <double> r(3,0);
-  vector <double> v(3,0);
-  vector <double> a(3,0);
-  vector <double> x(3);
-  vector <double> x0(3);
-  for (int i = 0; i < 3; i++) {
-    x0[i] = r_min;
-    x[i] = r_max;
-  }
-  Cube Root;
-  Root.set_x0(x0);
-  Root.set_x(x);
-  // SETTING POSITIONS AND VELOCITIES RANDOMLY
+  double dt = 0.0001;
+  double r_max = 100, r_min = -100, v_max = 2, v_min = -2, m_max = 700, m_min = 200;
+  int count = 0;
   for (int i = 0; i < n; i++) {
-    for (int j = 0; j < 3; j++) {
-      r[j] = random(r_max,r_min,4);
-      v[j] = random(v_max,v_min,4);
+    for (int j = 0; j < d; j++) {
+      System[i].r.push_back(random(r_max,r_min,p));
+      System[i].v.push_back(random(v_max,v_min,p));
+      System[i].f.push_back(0);
     }
-    m = random(m_max,m_min,4);
-    System[i].set_r(r);
-    System[i].set_v(v);
-    System[i].set_a(a);
-    System[i].set_m(m);
+    System[i].m = random(m_max,m_min,p);
   }
+  double E_tot0 = E_kin(System,n) + E_pot(System,n);
+  energy << "===================================================" << endl;
+  energy << "Initial Kinetic Energy        : " << E_kin(System,n) << endl;
+  energy << "Initial Potential Energy      : " << E_pot(System,n) << endl;
+  energy << "Total Energy of the System (i): " << E_tot0 << endl;
+  // LEAPFROG ALGORITHM
+  for (int i = 0; i < n; i++) {
+    for (int j = i + 1; j < n; j++) {
+      // Trying to get lower complexity < O(N^2)
+      System[i].f = s_vec(System[i].f,force(System[i],System[j]));
+      System[j].f = s_vec(System[j].f,force(System[j],System[i]));
+    } 
+  }
+  print_system(System,n,'r','s');
+  for (int t = 0; t < 100000000; t++) {
+    for (int i = 0; i < n; i++) {
+      if (t % 99 == 0) print_system(System,n,'r','r');
+      System[i].v = s_vec(System[i].v,p_vec(System[i].f,dt * 0.5 / System[i].m));
+      System[i].r = s_vec(System[i].r,p_vec(System[i].v,dt));
+      System[i].f = empty(System[i].f);
+      for (int j = i + 1; j < n; j++) {
+	System[i].f = s_vec(System[i].f,force(System[i],System[j]));
+	System[j].f = s_vec(System[j].f,force(System[j],System[i]));
+      } 
+      System[i].v = s_vec(System[i].v,p_vec(System[i].f,dt * 0.5 / System[i].m));
+    }
+  }
+  double E_tot = E_kin(System,n) + E_pot(System,n);
+  energy << "---------------------------------------------------" << endl;
+  energy << "Final Kinetic   Energy        : " << E_kin(System,n) << endl;
+  energy << "Final Potential Energy        : " << E_pot(System,n) << endl;
+  energy << "Total Energy of the System (f): " << E_tot << endl;
+  energy << "===================================================" << endl;
+  double error = (E_tot - E_tot0) / E_tot0 * 100;
+  if (error < 0) error *= -1;
+  energy << "Energy Error (%) => " << error                       << endl;
+  energy << "===================================================" << endl;
 }
